47b12e356ec9381c9cbaf6494c792147848ba115
Taikou Kobayashi <taikou.kobayashi@jointcrew.co.jp>
2019-08-05 19:01:26 +0900

    handleError

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,20 +5,44 @@ import { Observable, of } from "rxjs";
 import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
 import { MessageService } from "./message.service";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs/operators";
 
 @Injectable({ providedIn: "root" })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  private heroesUrl = "api/heroes";
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
     this.messageService.add("HeroService: fetched heroes");
-    return of(HEROES);
+    return this.http
+      .get<Hero[]>(this.heroesUrl)
+      .pipe(catchError(this.handleError<Hero[]>("getHeroes", [])));
   }
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: リモート上のロギング基盤にエラーを送信する
+      console.error(error); // かわりにconsoleに出力
+
+      // TODO: ユーザーへの開示のためにエラーの変換処理を改善する
+      this.log(`${operation} failed: ${error.message}`);
+
+      // 空の結果を返して、アプリを持続可能にする
+      return of(result as T);
+    };
   }
 }
