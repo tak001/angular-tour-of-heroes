e065d0334d912be7197f34f11b087d63fab783d4
Taikou Kobayashi <twitwitwi2@yahoo.co.jp>
2019-12-23 23:56:46 +0900

    feat:npm up & npm audit fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,11 +9,12 @@ const routes: Routes = [
   { path: "", redirectTo: "/dashboard", pathMatch: "full" },
   { path: "dashboard", component: DashboardComponent },
   { path: "detail/:id", component: HeroDetailComponent },
-  { path: "heroes", component: HeroesComponent }
+  { path: "heroes", component: HeroesComponent },
+  { path: '**', redirectTo: '/' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
